b4da0324c9d9fffad21c3d1b27a2e7777c7466c4
Aleksandr Sadykov <sumerokr@gmail.com>
2020-11-14 15:39:56 +0100

    Use aws deps

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import Amplify from "aws-amplify";
+import "@aws-amplify/ui-vue";
+import aws_exports from "./aws-exports";
+
+Amplify.configure(aws_exports);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
